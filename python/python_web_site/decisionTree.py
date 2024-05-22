c21be375be8cdadd7f9f59f30ef8f35b142eba70
lluaces83 <145365002+lluaces83@users.noreply.github.com>
2024-05-22 18:30:33 -0400

    machine learning

---
+++ python/python_web_site/decisionTree.py
@@ -0,0 +1,23 @@
+import pandas
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+
+df = pandas.read_csv("D:\portfolio\Academyfl\python\python_web_site\data3.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+print(dtree.predict([[40, 10, 7, 1]])) 
+
+print("[1] means 'GO'")
+print("[0] means 'NO'")
+
